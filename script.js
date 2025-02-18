8dd1f04be1cead2c2a10c2cc5031887b2a23f1ca
Eric Graham <wiltzie.eg@gmail.com>
2025-02-18 12:48:32 -0500

    Created index and script.js files

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1. Create three variables that will each calculate the 3 numbers of the combo using various operators.
+// 2. Display a message in console.log that will let the end user know what the alert is about.
+// 3. Setup an alert that displays the comb which is taken from the three variables using string interpolation.
+
+
+
+// Assigned three variables that each equal one of the combo numbers.
+const num1 = 2 * 5;
+const num2 = 20 + 20;
+const num3 = 50 - 11;
+
+// Printed a log message that displays a string.
+console.log("You have received this message because you have chosen to open an important vault.  Here is the secret combination:");
+
+// Created a alert message that displays the combo, which is the thre variables from above.
+alert(`The combo is ${num1}-${num2}-${num3}`);
